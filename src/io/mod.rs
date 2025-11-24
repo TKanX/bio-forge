7026b3dc343119109893b930411d7412acfff3ab
Tony Kan <tianchengkan@gmail.com>
2025-11-23 22:17:03 -0800

    refactor(io): Rearrange module declarations and exports for improved organization

--- src/io/mod.rs
+++ src/io/mod.rs
@@ -1,8 +1,21 @@
 mod context;
 mod error;
+mod mmcif;
+mod mol2;
+mod pdb;
 
-pub mod mmcif;
-pub mod mol2;
-pub mod pdb;
+pub use pdb::reader::read as read_pdb_structure;
+pub use pdb::writer::{
+    write_structure as write_pdb_structure, write_topology as write_pdb_topology,
+};
+
+pub use mmcif::reader::read as read_mmcif_structure;
+pub use mmcif::writer::{
+    write_structure as write_mmcif_structure, write_topology as write_mmcif_topology,
+};
+
+pub use mol2::reader::read as read_mol2_template;
+
+pub use context::IoContext;
 
 pub use error::Error;
