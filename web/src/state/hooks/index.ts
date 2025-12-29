881095fd57fc8f582385badfd9fdd0510e237cb9
Tony Kan <tianchengkan@gmail.com>
2025-12-29 00:46:38 -0800

    feat(web): Create hooks index file for exporting custom hooks

---
+++ web/src/state/hooks/index.ts
@@ -0,0 +1,8 @@
+/**
+ * @file Hooks exports
+ */
+
+export { useProcessor } from "./use-processor";
+export { useFileUpload } from "./use-file-upload";
+export { useKeyboardShortcuts } from "./use-keyboard-shortcuts";
+export { useWasmInit } from "./use-wasm-init";
