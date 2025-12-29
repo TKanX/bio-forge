f390d08b25334d01171379839d19f461044a8713
Tony Kan <tianchengkan@gmail.com>
2025-12-29 02:50:07 -0800

    feat(web): Add UI primitives exports for core components

---
+++ web/src/ui/primitives/index.ts
@@ -0,0 +1,34 @@
+/**
+ * @file UI primitives exports
+ */
+
+export { Button, type ButtonProps } from "./button";
+export { Input, type InputProps } from "./input";
+export { Switch, type SwitchProps } from "./switch";
+export { Select, type SelectProps, type SelectOption } from "./select";
+export { Checkbox, type CheckboxProps } from "./checkbox";
+export {
+  Card,
+  CardHeader,
+  CardContent,
+  CardFooter,
+  type CardProps,
+} from "./card";
+export { Badge, type BadgeProps, type BadgeVariant } from "./badge";
+export { Collapsible, type CollapsibleProps } from "./collapsible";
+export {
+  Dropdown,
+  IconButton,
+  type DropdownProps,
+  type DropdownItem,
+  type IconButtonProps,
+} from "./dropdown";
+export { Slider, type SliderProps } from "./slider";
+export { Textarea, type TextareaProps } from "./textarea";
+export {
+  Progress,
+  Spinner,
+  type ProgressProps,
+  type SpinnerProps,
+} from "./progress";
+export { Tooltip, type TooltipProps, type TooltipPosition } from "./tooltip";
