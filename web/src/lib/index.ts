cbb2f9a79725540cd8273470e08970967ceb233e
Tony Kan <tianchengkan@gmail.com>
2025-12-29 00:16:16 -0800

    feat(web): Implement library exports for utility functions and constants

---
+++ web/src/lib/index.ts
@@ -0,0 +1,26 @@
+/**
+ * @file Library exports
+ */
+
+export { cn } from "./cn";
+export {
+  formatFileSize,
+  formatNumber,
+  capitalize,
+  truncate,
+  pluralize,
+  debounce,
+} from "./utils";
+export {
+  APP_NAME,
+  GITHUB_URL,
+  NPM_URL,
+  DOCS,
+  MAX_VISIBLE_CHAINS,
+  ANIMATION,
+  ACCEPTED_STRUCTURE_EXTENSIONS,
+  ACCEPTED_TEMPLATE_EXTENSIONS,
+  ACCEPTED_EXTENSIONS,
+  SUPPORTED_EXTENSIONS,
+  MIME_TYPES,
+} from "./constants";
